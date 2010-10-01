9df6c9b946364134c7b4cb24773878662f9b9471
richardjohnwilkinson <richardjohnwilkinson@3d689674-af83-11de-99a6-4fa8e8bced8a>
2010-10-01 09:45:04 +0000

    adding component instantiation listener in application init
    git-svn-id: http://legup.googlecode.com/svn/trunk@547 3d689674-af83-11de-99a6-4fa8e8bced8a

--- wicket-wiquery/src/main/java/com/jweekend/WicketApplication.java
+++ wicket-wiquery/src/main/java/com/jweekend/WicketApplication.java
@@ -6,19 +6,23 @@ import org.odlabs.wiquery.core.commons.WiQueryInstantiationListener;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
- * 
+ *
  * @see wicket.myproject.Start#main(String[])
- * 
+ *
  * @author Richard Wilkinson - richard.wilkinson@jweekend.com
  *
  */
 public class WicketApplication extends WebApplication
-{    
+{
 	/**
 	 * Constructor
 	 */
 	public WicketApplication()
 	{
+	}
+
+	@Override
+	protected void init() {
 		addComponentInstantiationListener(new WiQueryInstantiationListener());
 	}
 
