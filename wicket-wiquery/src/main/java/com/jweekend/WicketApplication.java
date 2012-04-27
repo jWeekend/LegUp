1e447d1e3fffddef14621188466e36d2b2dc1a48
ernesto.reinaldo@jweekend.com <ernesto.reinaldo@jweekend.com@3d689674-af83-11de-99a6-4fa8e8bced8a>
2012-04-27 12:07:40 +0000

    got rid of javadoc warning
    git-svn-id: http://legup.googlecode.com/svn/trunk@725 3d689674-af83-11de-99a6-4fa8e8bced8a

--- wicket-wiquery/src/main/java/com/jweekend/WicketApplication.java
+++ wicket-wiquery/src/main/java/com/jweekend/WicketApplication.java
@@ -6,7 +6,6 @@ import org.apache.wicket.protocol.http.WebApplication;
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
  *
- * @see wicket.myproject.Start#main(String[])
  *
  * @author Richard Wilkinson - richard.wilkinson@jweekend.com
  *
