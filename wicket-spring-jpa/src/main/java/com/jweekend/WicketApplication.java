d6dde5c801dd2f37106bc11e8332d3385d5e89e0
ernesto.reinaldo@jweekend.com <ernesto.reinaldo@jweekend.com@3d689674-af83-11de-99a6-4fa8e8bced8a>
2012-05-09 06:13:20 +0000

    fix javadoc warnings
    git-svn-id: http://legup.googlecode.com/svn/trunk@729 3d689674-af83-11de-99a6-4fa8e8bced8a

--- wicket-spring-jpa/src/main/java/com/jweekend/WicketApplication.java
+++ wicket-spring-jpa/src/main/java/com/jweekend/WicketApplication.java
@@ -9,7 +9,7 @@ import com.jweekend.pages.EventPage;
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
  * 
- * @see wicket.myproject.Start#main(String[])
+ * @see Start#main(String[])
  * 
  * @author Richard Wilkinson - richard.wilkinson@jweekend.com
  *
