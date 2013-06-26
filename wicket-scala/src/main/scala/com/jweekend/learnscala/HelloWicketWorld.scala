a4fb4c1af7556d596392cd000b05ed5e8515ad40
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-06-27 00:02:46 +0300

    Update Wicket-Scala LegUp

--- wicket-scala/src/main/scala/com/jweekend/learnscala/HelloWicketWorld.scala
+++ wicket-scala/src/main/scala/com/jweekend/learnscala/HelloWicketWorld.scala
@@ -7,19 +7,23 @@ import org.apache.wicket.model.IModel
 import org.apache.wicket.markup.html.basic.Label
 
 class LegUp extends WebApplication {
-  def getHomePage = classOf[HomePage]
+	def getHomePage = classOf[HomePage]
 }
 
 class HomePage extends WebPage {
-  var name = "Hello!"
-  val form = new Form[String]("form")
-  add(form)
-  form.add(new TextField[String]("nameField", new ToySafeModel[String](name, { name = _ })))
-  form.add(new Label("nameLabel", new ToySafeModel[String](name , null)))
+	var name = "Hello!"
+	val form = new Form[String]("form")
+	add(form)
+	form.add(new TextField[String]("nameField", new ToySafeModel[String](name, { name = _ })))
+	form.add(new Label("nameLabel", new ToySafeModel[String](name , null)))
 }
 
-class ToySafeModel[T](accessor: => T, mutator: T => Unit) extends IModel[T]() {
-  override def getObject() = accessor
-  override def setObject(t: T) = mutator(t)
-  override def detach() = Nil
+class ToySafeModel[T](accessor: => T, mutator: T => Unit)
+  extends IModel[T]() {
+
+	override def getObject = accessor
+	override def setObject(t: T) {
+		mutator(t)
+	}
+	override def detach() {}
 }
